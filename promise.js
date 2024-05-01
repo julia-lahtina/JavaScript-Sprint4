53ceb05212d38090aed497ac611b45f4ee624b92
julia-lahtina <j.lakhtin@gmail.com>
2024-05-01 13:24:00 +0200

    add Example-2

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-let promise = new Promise((resolve, reject) => {
+/*let promise = new Promise((resolve, reject) => {
     setTimeout((response) => {
         if (response.httpSatus >= 200 && response.httpSatus < 400) {
             resolve(response.data)
@@ -8,5 +8,56 @@ let promise = new Promise((resolve, reject) => {
         }
     }, 2000, {httpSatus: 200, data: {userName: 'Dima', id: '4546456', status: 'active'}, error: {}})
 })
-
 console.log('1', promise)
+
+//----------------------------------------------------------------------------------------------------------------------
+// то, что описано выше, недостаточно для получения данных, нам нужны методы промисов: then, catch, finally
+// then - срабатывает тогда, когда у промиса меняется состояние с pending на resolved or rejected
+
+promise
+    .then(
+        res => {
+            console.log('res', res);
+            // return 10; //--------------------------> вернется не 10, а НОВЫЙ ОБЪЕКТ ПРОМИСА в состоянии resolve, поэтому после then можем еще then прописать
+        },
+        () => {
+        }
+    )
+    .then(res2 => {
+        console.log('res2', res2)
+    })*/
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Example-2
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout((response) => {
+        if (response.httpSatus >= 200 && response.httpSatus < 400) {
+            resolve(response.data)
+        } else {
+            reject(response.error)
+        }
+    }, 2000, {httpSatus: 404, data: {userName: 'Dima', id: '4546456', status: 'active'}, error: 'not found'})
+})
+
+
+promise
+    .then(
+        res => {
+            return new Promise((resolve, reject) => {
+                setTimeout((response) => {
+                    if (response.httpSatus >= 200 && response.httpSatus < 400) {
+                        resolve(response.data)
+                    } else {
+                        reject(response.error)
+                    }
+                }, 2000, {httpSatus: 200, data: {id: '4546456', count: 'DE5465456456465465454'}, error: {}})
+            })
+        },
+        (err) => {
+            console.log('err', err)
+        }
+    )
+    .then(res2 => {
+        console.log('res2', res2)
+    })
